f0a97886b6eed11520c8c816eb8b882bb466a468
LlyanaP <pollyanacostasousa@gmail.com>
2024-08-06 20:40:25 -0300

    Conectando o mangodb ao VsCode

---
+++ create-dadabase.mongodb.js
@@ -0,0 +1,12 @@
+/*Nome do Banco de Dados*/
+const database = 'BD3-POLLYANA-AULA';
+
+/*Nome do Banco da Coleçãoo de Dados*/
+const collection = 'LIVRARIA'
+
+/* Criar ou Acessar o Banco de Dados */
+use(database);
+
+/* Criar uma Coleção de Dados*/
+db.createCollection(collection);
+
