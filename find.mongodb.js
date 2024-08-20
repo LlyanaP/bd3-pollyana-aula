5e32acb0d64628b7ccc0451431a57a03d542bb2b
LlyanaP <pollyanacostasousa@gmail.com>
2024-08-20 20:26:55 -0300

    Inserção e seleção de dados

---
+++ find.mongodb.js
@@ -0,0 +1,11 @@
+/*Nome do Banco de Dados*/
+const database = 'BD3-POLLYANA-AULA';
+
+/*Nome do Banco da Coleçãoo de Dados*/
+const collection = 'LIVRARIA'
+
+/* Criar ou Acessar o Banco de Dados */
+use(database);
+
+/*Seleciona todos os registros da seleção*/
+db[collection].find()
